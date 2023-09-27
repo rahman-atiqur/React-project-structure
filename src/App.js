79f68d524ddadc057c45e98982aec95cdd68d591
Atiqur Rahman <atiqbit@gmail.com>
2023-09-26 23:31:07 -0400

    Commit-1 for React Project Structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
+import screenShot from '../src/assets/images/project-structure.PNG';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <div className="App">
+                <h2>-: Project-Structure :-</h2>
+                <img src={screenShot} alt="logo" width={500} height={800} />
+
+                {/* <img
+                    src={screenShot}
+                    className="App-logo"
+                    alt="logo"
+                   
+                /> */}
+            </div>
+        </>
+    );
 }
 
 export default App;
